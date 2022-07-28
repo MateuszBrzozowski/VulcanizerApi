72f4a928ec0138779b1a62a1ae3fa551cb0514dc
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-07-29 01:20:44 +0200

    impl SecurityConstant

--- src/main/java/pl/mbrzozowski/vulcanizer/constant/SecurityConstant.java
+++ src/main/java/pl/mbrzozowski/vulcanizer/constant/SecurityConstant.java
@@ -1,7 +1,7 @@
 package pl.mbrzozowski.vulcanizer.constant;
 
 public class SecurityConstant {
-    public static final long EXPIRATION_TIME = 5 * 24 * 60 * 60 * 1000; // miliseconds - now for tests 5 days
+    private static final long EXPIRATION_TIME = 432_000_000; // miliseconds - now for tests 5 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_TOKEN_HEADER = "Jwt-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
@@ -11,6 +11,5 @@ public class SecurityConstant {
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "Options";
-    public static final String[] PUBLIC_URLS = {"/users/login", "/users/register", "user/resetpassword/**", "/user/image/**", "/api/v1/business/recommend"};
-//    public static final String[] PUBLIC_URLS = { "**" };
+    public static final String[] PUBLIC_URLS = {"/user/login", "user/register", "user/resetpassword/**", "/user/image/**", "/"};
 }
