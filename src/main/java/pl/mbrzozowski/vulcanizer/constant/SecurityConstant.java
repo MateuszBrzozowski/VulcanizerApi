cfa22ed19675f65f08a5444f1e92cac6c4538419
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-07-30 02:01:36 +0200

    fix token verifier

--- src/main/java/pl/mbrzozowski/vulcanizer/constant/SecurityConstant.java
+++ src/main/java/pl/mbrzozowski/vulcanizer/constant/SecurityConstant.java
@@ -12,4 +12,5 @@ public class SecurityConstant {
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "Options";
     public static final String[] PUBLIC_URLS = {"/users/login", "/users/register", "user/resetpassword/**", "/user/image/**", "/api/v1/business/recommend"};
+//    public static final String[] PUBLIC_URLS = { "**" };
 }
