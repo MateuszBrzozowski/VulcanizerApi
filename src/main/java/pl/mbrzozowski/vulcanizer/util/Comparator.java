6df9825759afc0037680daba83d85e4aceb3740c
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-08-11 21:33:22 +0200

    testing comparator

--- src/main/java/pl/mbrzozowski/vulcanizer/util/Comparator.java
+++ src/main/java/pl/mbrzozowski/vulcanizer/util/Comparator.java
@@ -9,30 +9,44 @@ public class Comparator {
     public static boolean compare(Address address, AddressRequest addressRequest) {
         if (address != null && addressRequest != null) {
             if (address.getAddressLine() != null && addressRequest.getAddressLine() != null) {
-                if (!address.getAddressLine().equalsIgnoreCase(addressRequest.getAddressLine())) {
+                String addressLine = removeBlank(address.getAddressLine());
+                String addressRequestLine = removeBlank(addressRequest.getAddressLine());
+                if (!addressLine.equalsIgnoreCase(addressRequestLine)) {
                     return false;
                 }
             }
             if (address.getCity() != null && addressRequest.getCity() != null) {
-                if (!address.getCity().equalsIgnoreCase(addressRequest.getCity())) {
+                String city = removeBlank(address.getCity());
+                String cityRequest = removeBlank(addressRequest.getCity());
+                if (!city.equalsIgnoreCase(cityRequest)) {
                     return false;
                 }
             }
             if (address.getCode() != null && addressRequest.getCode() != null) {
-                if (!address.getCode().equalsIgnoreCase(addressRequest.getCode())) {
+                String code = removeBlank(address.getCode());
+                String codeRequest = removeBlank(addressRequest.getCode());
+                if (!code.equalsIgnoreCase(codeRequest)) {
                     return false;
                 }
             }
             if (address.getState() != null && addressRequest.getState() != null) {
-                if (!address.getState().getName().equalsIgnoreCase(addressRequest.getState())) {
+                String state = removeBlank(address.getState().getName());
+                String stateRequest = removeBlank(addressRequest.getState());
+                if (!state.equalsIgnoreCase(stateRequest)) {
                     return false;
                 }
             }
             if (address.getCountry() != null && addressRequest.getCountry() != null) {
-                return address.getCountry().equalsIgnoreCase(addressRequest.getCountry());
+                String country = removeBlank(address.getCountry());
+                String countryRequest = removeBlank(addressRequest.getCountry());
+                return country.equalsIgnoreCase(countryRequest);
             }
         }
         return true;
+    }
+
+    private static String removeBlank(String source) {
+        return source.replace(" ", "");
     }
 
     public static boolean compare(Phone phone, String phoneNumber) {
--- src/test/java/pl/mbrzozowski/vulcanizer/util/ComparatorTest.java
+++ src/test/java/pl/mbrzozowski/vulcanizer/util/ComparatorTest.java
@@ -1,7 +1,224 @@
 package pl.mbrzozowski.vulcanizer.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.mbrzozowski.vulcanizer.dto.AddressRequest;
+import pl.mbrzozowski.vulcanizer.entity.Address;
+import pl.mbrzozowski.vulcanizer.entity.State;
 
 class ComparatorTest {
 
+    @Test
+    void compareAddressAndAddressRequest_Same() {
+        Address address = Address.builder()
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameAddressLine() {
+        Address address = Address.builder()
+                .addressLine("Line")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .addressLine("line")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_NotSameAddressLine() {
+        Address address = Address.builder()
+                .addressLine("Line")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .addressLine("test")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertFalse(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameAddressLineWithBlank() {
+        Address address = Address.builder()
+                .addressLine("Line")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .addressLine("line ")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCity() {
+        Address address = Address.builder()
+                .city("City")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .city("City")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_NotSameCity() {
+        Address address = Address.builder()
+                .city("City")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .city("BigCity")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertFalse(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCityWithBlank() {
+        Address address = Address.builder()
+                .city("City")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .city("City ")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCode() {
+        Address address = Address.builder()
+                .code("99-999")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .code("99-999")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_NotSameCode() {
+        Address address = Address.builder()
+                .code("99-999")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .code("88-889")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertFalse(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCodeWithBlank() {
+        Address address = Address.builder()
+                .code("99-999")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .code("99-999 ")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameState() {
+        State state = new State("State");
+        Address address = Address.builder()
+                .state(state)
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .state("State")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_NotSameState() {
+        State state = new State("State");
+        Address address = Address.builder()
+                .state(state)
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .state("States")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertFalse(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameStateWithBlank() {
+        State state = new State("State");
+        Address address = Address.builder()
+                .state(state)
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .state("State ")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCountry() {
+        Address address = Address.builder()
+                .country("Country")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .country("Country")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_NotSameCountry() {
+        Address address = Address.builder()
+                .country("Country")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .country("SmallCountry")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertFalse(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameCountryWithBlank() {
+        Address address = Address.builder()
+                .country("Country")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .country("Country ")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
+
+    @Test
+    void compareAddressAndAddressRequest_SameAllAddress() {
+        State state = new State("State");
+        Address address = Address.builder()
+                .addressLine("Line")
+                .city("city")
+                .code("99-999")
+                .state(state)
+                .country("Country")
+                .build();
+        AddressRequest addressRequest = AddressRequest.builder()
+                .addressLine("line ")
+                .city("city")
+                .code("99-999")
+                .state("state")
+                .country("Country")
+                .build();
+        boolean compare = Comparator.compare(address, addressRequest);
+        Assertions.assertTrue(compare);
+    }
 }
