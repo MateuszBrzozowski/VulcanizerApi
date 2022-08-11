36f4e174e035b1fe0201d7f7495a6ee6afc7de3b
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-08-11 20:26:03 +0200

    impl comparator compare for address and address request

---
+++ src/main/java/pl/mbrzozowski/vulcanizer/util/Comparator.java
@@ -0,0 +1,36 @@
+package pl.mbrzozowski.vulcanizer.util;
+
+import pl.mbrzozowski.vulcanizer.dto.AddressRequest;
+import pl.mbrzozowski.vulcanizer.entity.Address;
+
+public class Comparator {
+
+    public static boolean compare(Address address, AddressRequest addressRequest) {
+        if (address != null && addressRequest != null) {
+            if (address.getAddressLine() != null && addressRequest.getAddressLine() != null) {
+                if (!address.getAddressLine().equalsIgnoreCase(addressRequest.getAddressLine())) {
+                    return false;
+                }
+            }
+            if (address.getCity() != null && addressRequest.getCity() != null) {
+                if (!address.getCity().equalsIgnoreCase(addressRequest.getCity())) {
+                    return false;
+                }
+            }
+            if (address.getCode() != null && addressRequest.getCode() != null) {
+                if (!address.getCode().equalsIgnoreCase(addressRequest.getCode())) {
+                    return false;
+                }
+            }
+            if (address.getState() != null && addressRequest.getState() != null) {
+                if (!address.getState().getName().equalsIgnoreCase(addressRequest.getState())) {
+                    return false;
+                }
+            }
+            if (address.getCountry() != null && addressRequest.getCountry() != null) {
+                return address.getCountry().equalsIgnoreCase(addressRequest.getCountry());
+            }
+        }
+        return true;
+    }
+}
